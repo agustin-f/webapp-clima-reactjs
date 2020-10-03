c40772c574c6813c82b87c51666902854eaa11fd
agustin-f <farisanoagustin@gmail.com>
2020-10-03 17:43:16 -0300

    mi primer commit con la configuracion del entorno de desarrollo en reactjs

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './index.css';
+
+render( < h1 > Esta es una app en react para ver el clima de las ciudades del mundo < /h1>,document.getElementById('app'));
